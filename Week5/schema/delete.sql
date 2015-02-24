f71f689c817afd54cd3a963b8dc0634cc9158ec9
ProfessorBrunner <professor.brunner@gmail.com>
2015-02-24 17:39:31 -0600

    Final Week5

---
+++ Week5/schema/delete.sql
@@ -0,0 +1,4 @@
+DELETE FROM flights WHERE flights.originCode == 'Origin' ;
+DELETE FROM carriers WHERE carriers.uniqueCode =='Code' ;
+DELETE FROM airports WHERE airports.code =='iata' ;
+DELETE FROM airplanes WHERE tailNumber =='tailnum' ;
